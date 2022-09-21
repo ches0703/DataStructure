840866990ec541b4af0bd0b5e3062c60d934c2c4
ches0703 <ches0703@naver.com>
2022-09-21 18:43:14 +0900

    Training01 finish

--- DataStructure/HomeWork01.c
+++ DataStructure/HomeWork01.c
@@ -25,27 +25,27 @@ int main()
 
 	// 1.1 1 ~ 100,000의 정수로 이루어진 역순정렬된 배열 생성 및 정렬, 정렬 검사 수행
 
-	//printf("1.1. 역순 정렬 배열(100,000 ~ 0) 생성 및 정렬, 정렬 검사 수행\n");
+	printf("1.1. 역순 정렬 배열(100,000 ~ 1) 생성 및 정렬, 정렬 검사 수행\n");
 
-	//printf("1.1.1. 역순 배열 생성 : ");
-	//reverse_list_make(list, 100000);
-	//printf("생성 완료\n");
+	printf("1.1.1. 역순 배열 생성 : ");
+	reverse_list_make(list, 100000);
+	printf("생성 완료\n");
 
-	//printf("1.1.2. 배열 정렬 : ");
-	//sort(list, 100000, &duration);
-	//printf("정렬 완료\n");
+	printf("1.1.2. 배열 정렬 : ");
+	sort(list, 100000, &duration); 
+	printf("정렬 완료\n");
+	printf("정렬에 걸린 시간 : %.3fs\n", duration/1000);
 
-	//printf("정렬에 걸린 시간 : %.4fs\n", duration/1000);
+	printf("1.1.3. 정렬 상태 : ");
+	is_sorted(list, 100000);
 
-	//printf("1.1.3. 정렬 상태 : ");
-	//is_sorted(list, 100000);
+	printf("배열 출력 :\n");
+	print_list(list, 100000);
 
-	//printf("배열 출력 :\n");
-	//print_list(list, 100000);
 
 	// 1.2 역순 정렬된 배열의 크기를 100부터 1,000,000까지 바꾸면서 sort 함수의 실행 시간을 측정하여 관찰지에 기록
 	
-	/*printf("1.2 역순 정렬된 배열의 크기를 100부터 1,000,000까지 바꾸면서 sort 함수의 실행 시간을 측정하여 관찰지에 기록\n");
+	printf("1.2 역순 정렬된 배열의 크기를 0부터 1,000,000까지 바꾸면서 sort 함수의 실행 시간을 측정하여 관찰지에 기록\n");
 	printf("      n        time\t   sort\n");
 	for (int i = 0; i < 6;i++) {
 		int n;
@@ -54,7 +54,7 @@ int main()
 		case 0:
 			reverse_list_make(list, 0);
 			sort(list, 0, &duration);
-			printf("%7d %10.4fs     ", 0, duration/1000);
+			printf("%7d %10.3fs     ", 0, duration/1000);
 			is_sorted(list, 0);
 			n = 10;
 			break;
@@ -80,22 +80,21 @@ int main()
 			for (int j = 1; j < 10; j++) {
 				reverse_list_make(list, n * j);
 				sort(list, n * j, &duration);
-				printf("%7d %10.4fs     ", n * j, duration/1000);
+				printf("%7d %10.3fs     ", n * j, duration/1000);
 				is_sorted(list, n * j);
 			}
 		}
 		else {
 			reverse_list_make(list, n);
 			sort(list, n, &duration);
-			printf("%7d %10.4fs     ", n, duration / 1000);
+			printf("%7d %10.3fs     ", n, duration / 1000);
 			is_sorted(list, n);
 		}
-		
-	}*/
+	}
 
 	// 1.3 역순 정렬 배열(100,000 ~ 0) 생성 및 정렬, 이진검색 수행
 
-	printf("1.3. 역순 정렬 배열(100,000 ~ 0) 생성 및 정렬, 이진검색 수행\n");
+	printf("1.3. 역순 정렬 배열(100,000 ~ 1) 생성 및 정렬, 이진검색 수행\n");
 
 	printf("1.3.1. 역순 배열 생성 : ");
 	reverse_list_make(list, 100000);
@@ -104,39 +103,66 @@ int main()
 	printf("1.3.2. 배열 정렬 : ");
 	sort(list, 100000, &duration);
 	printf("정렬 완료\n");
-
-	printf("정렬에 걸린 시간 : %.4fs\n", duration/1000);
+	printf("정렬에 걸린 시간 : %.3fs\n", duration/1000);
 
 	printf("1.3.3. 정렬 상태 : ");
 	is_sorted(list, 100000);
 	
-
-
 	for (;;){
 		printf("검색 데이터( 1 ~ 100000 ) : ");
 		scanf("%d", &search_data);
 		if (search_data == -1) {
-			printf("검색 종료");
+			printf("검색 종료\n");
 			break;
 		}
 		printf("검색 데이터 : %6d\t Counter : %d\n",list[binsearch(list, search_data, 0, 100000-1, &search_count)],search_count);
 	}
 
+
+	// 1.4 역순 정렬 배열(50,000 ~ 0) 생성 및 정렬, 이진검색 수행 후 n = 100,000의 배열과 비교
+
+	printf("1.4. 역순 정렬 배열(50,000 ~ 1) 생성 및 정렬, 이진검색 수행\n");
+
+	printf("1.4.1. 역순 배열 생성 : ");
+	reverse_list_make(list, 50000);
+	printf("생성 완료\n");
+
+	printf("1.4.2. 배열 정렬 : ");
+	sort(list, 50000, &duration);
+	printf("정렬 완료\n");
+	printf("정렬에 걸린 시간 : %.3fs\n", duration / 1000);
+
+	printf("1.4.3. 정렬 상태 : ");
+	is_sorted(list, 50000);
+
+	for (;;) {
+		printf("검색 데이터( 1 ~ 50000 ) : ");
+		scanf("%d", &search_data);
+		if (search_data == -1) {
+			printf("검색 종료\n");
+			break;
+		}
+		printf("검색 데이터 : %6d\t Counter : %d\n", list[binsearch(list, search_data, 0, 50000 - 1, &search_count)], search_count);
+	}
+
+
 	return 0;
 
-
 }
+// 역순 정렬 배열 생성
 void reverse_list_make(int list[], int n) {
 	for (int i = 0; i < n; i++) {
 		list[i] = n - i;
 	}
 }
+// 배열 출력
 void print_list(int list[], int n) {
 	for (int i = 0; i < n; i++) {
 		printf("%7d ", list[i]);
 	}
 	printf("\n-----배열 출력 완료------\n");
 }
+// 배열 정렬
 void sort(int list[], int n, double* duration) {
 	int i, j, min, temp;
 	clock_t start, stop;
@@ -152,6 +178,7 @@ void sort(int list[], int n, double* duration) {
 	stop = clock();
 	*duration = (double)(stop - start);
 }
+// 배열 정렬 상태 확인
 void is_sorted(int list[], int n) {
 	for (int i = 0; i < n; i++) {
 		for (int j = i + 1; j < n; j++) {
@@ -164,6 +191,7 @@ void is_sorted(int list[], int n) {
 	printf("정렬 됨\n");
 	return;
 }
+// 이진 검색
 int binsearch(int list[], int searchnum, int left, int right, int *search_count) {
 	int middle;
 	*search_count = 0;
