c690dd8415f164225fccb6d52731d73ea83712a8
ches0703 <ches0703@naver.com>
2022-12-09 03:02:02 +0900

    HW12 12./08

--- DataStructure/HomeWork12.c
+++ DataStructure/HomeWork12.c
@@ -3,9 +3,9 @@
 #include<stdlib.h>
 #include<time.h>
 # define SWAP(x,y,t) ((t)=(x), (x)=(y), (y)=(t)) 
-
-void printArray(double* arr, int n);
+int count = 0;
 double* makeRnadArray(int n);
+double* makeDesendingArray(int n);
 double* copyArray(double* arr, int n);
 int sortCheck(double* arr, int n);
 double selectionSort(double* arr, int n);
@@ -15,35 +15,88 @@ double quickSort(double* arr, int n, int left, int right);
 int main()
 {
 	srand((unsigned)time(NULL));
-
+	double* original_arr;
+	double* copy_arr;
 	double duration;
 	int n;
-	n = 10;
-	double* original_arr = makeRnadArray(n);
-	double* copy_arr = copyArray(original_arr, n);
 
-	printArray(original_arr, n);
-	duration = quickSort(original_arr, n, 0, n - 1);
+	// Rand case
+	printf("Rand Case\n");
+	for (;;) {
+		printf("Enter the array's size :");
+		scanf("%d", &n);
+		if (n < 0) {
+			break;
+		}
+		original_arr = makeRnadArray(n);
 
-	printf("duration : %f\n", duration);
-	printArray(original_arr, n);
+		// Seletion Sort
+		printf("Seletion Sort\n");
+		copy_arr = copyArray(original_arr, n);
+		duration = selectionSort(copy_arr, n);
+		printf("duration : %f\n", duration);
+		printf("Is Sorted? : %s\n", (sortCheck(copy_arr, n) ? "True" : "False"));
+		free(copy_arr);
+
+		// Insertion Sort
+		printf("Insertion Sort\n");
+		copy_arr = copyArray(original_arr, n);
+		duration = insertionSort(copy_arr, n);
+		printf("duration : %f\n", duration);
+		printf("Is Sorted? : %s\n", (sortCheck(copy_arr, n) ? "True" : "False"));
+		free(copy_arr);
+
+		// Quick Sort
+		printf("Quick Sort\n");
+		copy_arr = copyArray(original_arr, n);
+		duration = quickSort(copy_arr, n, 0, n - 1);
+		printf("duration : %f\n", duration);
+		printf("Is Sorted? : %s\n", (sortCheck(copy_arr, n) ? "True" : "False"));
+		free(copy_arr);
+
+		free(original_arr);
+	}
+
+	// Worst case
+	printf("Worst Case\n");
+	for (;;) {
+		printf("Enter the array's size :");
+		scanf("%d", &n);
+		if (n < 0) {
+			break;
+		}
+
+		original_arr = makeDesendingArray(n);
+
+		// Seletion Sort
+		printf("Seletion Sort\n");
+		copy_arr = copyArray(original_arr, n);
+		duration = selectionSort(copy_arr, n);
+		printf("duration : %f\n", duration);
+		printf("Is Sorted? : %s\n", (sortCheck(copy_arr, n) ? "True" : "False"));
+		free(copy_arr);
+
+		// Insertion Sort
+		printf("Insertion Sort\n");
+		copy_arr = copyArray(original_arr, n);
+		duration = insertionSort(copy_arr, n);
+		printf("duration : %f\n", duration);
+		printf("Is Sorted? : %s\n", (sortCheck(copy_arr, n) ? "True" : "False"));
+		free(copy_arr);
+
+		// Quick Sort
+		printf("Quick Sort\n");
+		copy_arr = copyArray(original_arr, n);
+		duration = quickSort(copy_arr, n, 0, n - 1);
+		printf("duration : %f\n", duration);
+		printf("Is Sorted? : %s\n", (sortCheck(copy_arr, n) ? "True" : "False"));
+		free(copy_arr);
+
+		free(original_arr);
+	}
+
 	
 
-
-}
-void printArray(double* arr, int n) {
-	for (int i = 0; i < n; i++) {
-		printf("[%.3f]", arr[i]);
-	}
-	printf("\n\n");
-}
-
-double* copyArray(double* arr, int n) {
-	double* copy_arr = (double*)malloc(sizeof(double) * n);
-	for (int i = 0; i < n; i++) {
-		copy_arr[i] = arr[i];
-	}
-	return copy_arr;
 }
 
 double* makeRnadArray(int n) {
@@ -52,6 +105,22 @@ double* makeRnadArray(int n) {
 		arr[i] = (rand() / (0x7fff * 1.0)) * 10;
 	}
 	return arr;
+}
+
+double* makeDesendingArray(int n) {
+	double* arr = (double*)malloc(sizeof(double) * n);
+	for (int i = 0; i < n; i++) {
+		arr[i] = n--;
+	}
+	return arr;
+}
+
+double* copyArray(double* arr, int n) {
+	double* copy_arr = (double*)malloc(sizeof(double) * n);
+	for (int i = 0; i < n; i++) {
+		copy_arr[i] = arr[i];
+	}
+	return copy_arr;
 }
 
 int sortCheck(double* arr, int n) {
@@ -64,9 +133,9 @@ int sortCheck(double* arr, int n) {
 
 double selectionSort(double* arr, int n) {
 	clock_t start, end;
+	start = clock();
 	int min_index;
 	double temp;
-	start = clock();
 	for (int i = 0; i < n; i++) {
 		min_index = i;
 		for (int j = i + 1; j < n; j++) {
@@ -80,8 +149,8 @@ double selectionSort(double* arr, int n) {
 
 double insertionSort(double* arr, int n) {
 	clock_t start, end;
-	double insert_value;
 	start = clock();
+	double insert_value;
 	for (int i = 1, j; i < n; i++) {
 		insert_value = arr[i];
 		for (j = i - 1; (j >= 0) && (insert_value < arr[j]); j--) {
@@ -94,63 +163,26 @@ double insertionSort(double* arr, int n) {
 }
 
 double quickSort(double* arr, int n, int left, int right) {
-	if (left >= right) { printf("Return\n");  return 0; }
-	printf("n = %d, left = %d, right = %d\n", n, left, right);
 	clock_t start, end;
-	double standard = arr[left + right / 2];
-	double temp;
 	start = clock();
-	int l, r;
-	int s;
-	printf("stadard = %.3f\n", standard);
-	for (l = left, r = right; (l < r) && (l < n);) {
-
-
-		printf("left = %d, right = %d\n", l, r);
-		printArray(arr, n);
-		scanf("%d", &s);
-		if (arr[l] < standard) {
+	if ((left >= right) || (n <= 0)) { return 0; }
+	int l = left + 1, r = right;
+	int pivot = left;
+	int new_pivot = left;
+	double temp;
+	for (; l <= r;) {
+		if (arr[pivot] > arr[l]) {
 			l++;
-			continue;
-		}
-		if (arr[r] > standard) {
-			r--;
-			continue;
-		}
-		if (arr[l] >= standard) {
-			printf("l[%.3f] <-> [%.3f]\n", arr[l], arr[r]);
-			SWAP(arr[l], arr[r], temp);
-			l++; r--;
-		}
-
-		
-
-		/*
-		if (arr[i] < arr[standard]) {
-			
-			printf("l[%.3f] <-> [%.3f]\n", arr[i], arr[l]);
-			SWAP(arr[i], arr[l], temp);
-			l++;
-
+			new_pivot++;
 		}
 		else {
-			for (; arr[r] >= arr[standard]; r--) { ; }
-			printf("r[%.3f] <-> [%.3f]\n", arr[i], arr[r]);
-						r--;
+			SWAP(arr[r], arr[l], temp);
+			r--;
 		}
-		*/
 	}
-	printf("Soting end\n");
-	printArray(arr, n);
-
-	printf("Left part : ");
-	printf("n = %d, left = %d, right = %d\n", l, left, l - 1);
-
-	printf("Right part : ");
-	printf("n = %d, left = %d, right = %d\n", n - r, l, right);
-
-	quickSort(arr, l, left, l - 1);
-	quickSort(arr, n - r - 1, l, right);
+	SWAP(arr[pivot], arr[new_pivot], temp);
+	quickSort(arr, new_pivot - left, left, new_pivot - 1);
+	quickSort(arr, right - new_pivot, new_pivot + 1, right);
 	end = clock();
 	return (double)(end - start) / 1000;
 }
